85a9dcb57e141a2504a10a85b641702fa6ddd390
Elizabeth Lopez Farias <160768836+ElizabethLopezFarias@users.noreply.github.com>
2024-03-06 23:41:27 -0300

    simplificacion

--- fuerza_bruta.py
+++ fuerza_bruta.py
@@ -1,35 +1,28 @@
 #El programa fuerza_bruta.py debe intentar todas las combinaciones de letras posibles, en
 #orden alfabético, hasta que la combinación de letras sea igual a la de la contraseña indicada.
 #Deberá hacer este proceso letra por letra, de izquierda a derecha.
-
-#Importación de Bibliotecas
-from string import ascii_lowercase #contiene todas las letras minúsculas del alfabeto inglés
-from itertools import product #realiza el producto cartesiano de los iterables proporcionados
-import getpass # proporciona una forma segura de ingresar contraseñas sin mostrar la entrada en pantalla
+#Simplificado
+#Importación de Biblioteca
+from string import ascii_lowercase
 
 print("Bienvenido al programa de fuerza bruta para contraseñas.")
 
-# Solicitar al usuario que ingrese la contraseña y la oculta
-contrasena_oculta = getpass.getpass("Ingresa la contraseña (sin la ñ): ")
-
-# Convertir la contraseña a minúsculas
-contrasena_oculta = contrasena_oculta.lower()
+# Solicitar al usuario que ingrese la contraseña
+contrasena = input("Ingresa la contraseña, sin espacios ni ñ: ").strip().lower()
 
 # Inicializar variables
 intentos = 0
 combinacion_actual = ""
 
-# Obtener la longitud de la contraseña
-longitud_contrasena = len(contrasena_oculta)
-
 # Iterar a través de todas las combinaciones posibles
-# La función product genera todas las combinaciones posibles de letras minúsculas con la longitud especificada. 
+for caracter in contrasena:
+    for caracter_comparativo in ascii_lowercase:
+        intentos += 1
+        if caracter == caracter_comparativo:
+            combinacion_actual = ''.join(caracter)
+            break
 
-for combinacion in product(ascii_lowercase, repeat=longitud_contrasena):
-    intentos += 1
-    combinacion_actual = ''.join(combinacion) #une la cadena de caracteres para ir comparando la cadena ingresada con la comparativa
-
-    if combinacion_actual == contrasena_oculta:
+    if combinacion_actual == contrasena:
         break
 
 print(f"Se adivinó la contraseña en {intentos} intentos.")
